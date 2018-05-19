c608afe9c3ad986e22ff3fb9afc2de40753341c9
jok405 <john.y.kim84@gmail.com>
2018-05-18 23:01:32 -0400

    login form client-side validation

---
+++ react-page-setup/src/components/forms/LoginForm.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Form, Button, } from "semantic-ui-react";
+import Validator from 'validator';
+import InlineError from '../messages/InlineError';
+import PropTypes from 'prop-types';
+
+
+class LoginForm extends React.Component {
+    state = {
+        data: {},
+        loading: false,
+        errors: {}
+    };
+
+
+
+
+
+//*************************universal login change handler****************
+
+
+    onChange = e => 
+        this.setState({ 
+            data: { ...this.state.data, [e.target.name]: e.target.value}
+    });
+
+    onSubmit = () => {
+        const errors = this.validate(this.state.data);
+        this.setState({ errors });
+    };
+//*************************universal login change handler              */
+
+    validate = (data) => {
+        const errors = {};
+        if(!Validator.isEmail(data.email)) errors.email = "Invalid email";
+        if(!data.password) errors.password = "Cannot be empty";
+        return errors;
+    };
+
+    render() {
+        const { data, errors } = this.state;
+        return (
+            <Form>
+                <Form.Field error={!!errors.email}>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="example@example.com"
+                        value={data.email}
+                        onChange={this.onChange}
+                    />
+                    {errors.email && <InlineError text={errors.email}/>}
+                </Form.Field>
+                <Form.Field error={!!errors.password}>
+                    <label htmlFor="Password">Password</label>
+                    <input
+                        type="Password"
+                        id="Password"
+                        name="Password"
+                        placeholder="You are gonna forget - no really, you will"
+                        value={data.Password}
+                        onChange = {this.onChange}
+                    />
+                    {errors.password && < InlineError text = {errors.password}/>}
+                </Form.Field>
+                <Button primary>Login</Button>
+            </Form>
+        );
+    }
+}
+
+LoginForm.propTypes = {
+    submit: PropTypes.func.isRequired
+};
+
+
+export default LoginForm;
