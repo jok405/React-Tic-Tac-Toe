e78fe30c50d266872271be4b132cfe4cdf1bd51e
jok405 <john.y.kim84@gmail.com>
2018-05-18 22:02:40 -0400

    adding semantic ui

--- react-page-setup/src/components/pages/HomePage.js
+++ react-page-setup/src/components/pages/HomePage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
-const HomePage = () => ( 
+const HomePage = () => (
     <div>
         <h1> Home Page </h1>
         <Link to="/login">Login</Link>
