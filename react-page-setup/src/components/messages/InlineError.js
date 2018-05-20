d43e0973c85dbc5c369353049f26a5f15ceccbea
jok405 <john.y.kim84@gmail.com>
2018-05-19 22:55:28 -0400

    redux implementation

--- react-page-setup/src/components/messages/InlineError.js
+++ react-page-setup/src/components/messages/InlineError.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
 const InlineError = ({ text }) => (
     <span style={{ color: "#ae5856" }}>{text}</span>
 );
 
 InlineError.propTypes = {
-    text: Proptypes.string.isRequired
+    text: propTypes.string.isRequired
 };
 
 export default InlineError;
--- react-page-setup/src/index.js
+++ react-page-setup/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import "semantic-ui-css/semantic.min.css";
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'redux-react';
+import thunk from "redux-thunk";
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <BrowserRouter>
-        <App/>
+        <Provider store={store}>
+        <App />
+        </Provider>
     </BrowserRouter>,
     document.getElementById('root')
 );
